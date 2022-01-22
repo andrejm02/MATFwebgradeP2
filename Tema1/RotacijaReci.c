b54b3d0f465d4d2b0c8a9b8d64a428a4a27963af
andrejm02 <57645580+andrejm02@users.noreply.github.com>
2022-01-22 03:55:13 +0100

    Create RotacijaReci.c

---
+++ Tema1/RotacijaReci.c
@@ -0,0 +1,48 @@
+//Sa standardnog ulaza se učitava ime tekstualne datoteke i nenegativan broj k. Podrazumevati da će svaka reč u datoteci biti dužine najviše 127 karaktera. 
+//Napisati program koji će svaku reč iz ove datoteke rotirati za k mesta ulevo i tako dobijene reči upisati u datoteku čiji je naziv rotirano.txt. 
+//U slučaju greške na standardni izlaz za greške ispisati -1. Maksimalna dužina naziva datoteke je 63 karaktera.
+#include <stdio.h>
+#include <stdlib.h>
+#include <ctype.h>
+#include <string.h>
+
+void greska(){
+    fprintf(stderr,"-1\n");
+    exit(EXIT_FAILURE);
+}
+
+int main(){
+    char ulaz[63],rec[127];
+    int k;
+    scanf("%s %d",ulaz,&k);
+    FILE* fulaz, *izlaz;
+
+    fulaz = fopen(ulaz,"r");
+    izlaz = fopen("rotirano.txt","w");
+
+    if(fulaz == NULL || izlaz == NULL || k < 0){
+        greska();
+    }
+    
+    while(fscanf(fulaz,"%s",rec) != EOF){
+        if(strlen(rec) > k){
+            for(int i = k; i < strlen(rec);i++){
+                fprintf(izlaz,"%c",rec[i]);
+                if(i == strlen(rec)-1)
+                    for(int j = 0; j < k; j++)
+                        fprintf(izlaz,"%c",rec[j]);
+            }
+        }else if(strlen(rec) == k){
+            for(int i = 0; i < strlen(rec); i++){
+                fprintf(izlaz,"%c",rec[i]);
+            }
+        }else
+            for(int i = strlen(rec)-1; i >= 0; i--){
+                fprintf(izlaz,"%c",rec[i]);
+            }
+        fprintf(izlaz," ");
+    }
+
+    fclose(fulaz);
+    fclose(izlaz);
+}
